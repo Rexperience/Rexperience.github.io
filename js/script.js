30b628bd4842649853f3cdcd083fe3610f93becc
Ariiq Dio f <findmeanyway@gmail.com>
2014-08-31 17:01:16 +0100

    experimental_june

---
+++ js/script.js
@@ -0,0 +1,46 @@
+/*smooth scrolling*/
+
+$(function() {
+
+	$('a[href*=#]:not([href=#])').click(function() {
+		if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
+
+			var target = $(this.hash);
+			target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+			if (target.length) {
+				$('html,body').animate({
+					scrollTop: target.offset().top
+				}, 2000);
+				return false;
+			}
+		}
+	});
+
+});
+
+/* Remodal */
+<script>
+    $(document).on('open', '.remodal', function () {
+        console.log('open');
+    });
+
+    $(document).on('opened', '.remodal', function () {
+        console.log('opened');
+    });
+
+    $(document).on('close', '.remodal', function () {
+        console.log('close');
+    });
+
+    $(document).on('closed', '.remodal', function () {
+        console.log('closed');
+    });
+
+    $(document).on('confirm', '.remodal', function () {
+        console.log('confirm');
+    });
+
+    $(document).on('cancel', '.remodal', function () {
+        console.log('cancel');
+    });
+	</script>
